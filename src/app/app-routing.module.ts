bc950781a7365d745574963852b21af28930009a
carlosArruda1996 <edu.arruda.759@gmail.com>
2024-10-03 12:01:09 -0300

    correçoes de bugs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {DocumentacoesComponent} from './pages/documentacoes/documentacoes.component';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'documentacoes', component : DocumentacoesComponent},
-  {path : '', component: CardComponent},
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeadComponent } from './components/head/head.component';
-import { FootComponent } from './components/foot/foot.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import{ApresentacaoComponent} from './components/head/apresentacao/apresentacao.component';
 import { DocumentacoesComponent } from './pages/documentacoes/documentacoes.component';
@@ -25,7 +23,6 @@ import { RoadMapsComponent } from './pages/road-maps/road-maps.component';
     CardComponent,
     HomeComponent,
     HeadComponent,
-    FootComponent,
     MenuBarComponent,
     ApresentacaoComponent,
     DocumentacoesComponent,
