e3e31c378c3e081bf1d5430387ded5d905b42456
carlosArruda1996 <edu.arruda.759@gmail.com>
2024-10-04 10:22:35 -0300

    routes created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,19 @@ import {DocumentacoesComponent} from './pages/documentacoes/documentacoes.compon
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ArtigosComponent } from './pages/artigos/artigos.component';
+import { BibliotecaComponent } from './pages/biblioteca/biblioteca.component';
+import { RoadMapsComponent } from './pages/road-maps/road-maps.component';
+import { FaleConoscoComponent } from './pages/fale-conosco/fale-conosco.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'artigos', component : ArtigosComponent},
   {path: 'documentacoes', component : DocumentacoesComponent},
+  {path: 'biblioteca', component : BibliotecaComponent},
+  {path: 'road-maps', component : RoadMapsComponent},
+  {path: 'fale-conosco', component : FaleConoscoComponent},
   
 ];
 
